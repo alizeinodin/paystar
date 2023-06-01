0532bc187a76746c1bdc5130d64ff74a59ff16f2
alizeinodin <alizeinodin79@gmail.com>
2023-06-01 20:04:54 +0430

    OrderFactory add

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'amount' => $this->faker->numberBetween(1, 100000),
+            'ref_num' => $this->faker->text(10),
+        ];
+    }
+}
