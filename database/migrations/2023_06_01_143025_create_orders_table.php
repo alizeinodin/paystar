8bf4d4fe1a52fb17583e53bf3914f8c2ced3daa1
alizeinodin <alizeinodin79@gmail.com>
2023-06-01 19:29:26 +0430

    foreignId add on orders table for one to many relationship

--- database/migrations/2023_06_01_143025_create_orders_table.php
+++ database/migrations/2023_06_01_143025_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,7 @@ return new class extends Migration {
             $table->string('status')->default('pending');
             $table->string('transaction_id')->nullable();
             $table->string('tracking_code')->nullable();
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
