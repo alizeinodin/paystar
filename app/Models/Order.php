6c95aee264f3e72ee650591e79a5ebe0de397faa
alizeinodin <alizeinodin79@gmail.com>
2023-06-01 19:15:01 +0430

    Order model add

---
+++ app/Models/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'amount',
+        'status',
+        'ref_num',
+        'transaction_id',
+        'tracking_code',
+    ];
+
+    protected $casts = [
+        'status' => OrderStatus::class
+    ];
+}
