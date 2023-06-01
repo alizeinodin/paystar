c2a18cd705cfb4570f4260b4adc6ff4f7b4a17a1
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 01:12:28 +0430

    Create method add in Payment Trait

--- app/Traits/Payment.php
+++ app/Traits/Payment.php
@@ -3,6 +3,8 @@
 namespace App\Traits;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\StreamInterface;
 
 /*
  * The Payment Trait is for Connection with PayStar
@@ -11,6 +13,7 @@ use GuzzleHttp\Client;
 trait Payment
 {
     private Client $client;
+    private string $algorithm = "SHA512";
 
     public function __construct()
     {
@@ -22,6 +25,35 @@ trait Payment
                 'Authorization' => "Bearer " . env('PAYSTAR_GATEWAY_ID')
             ],
         ]);
+    }
+
+    private function sign(int $amount, int $order_id, string $callback): bool|string
+    {
+        $data = $amount . "#" . $order_id . "#" . $callback;
+        return hash_hmac($this->algorithm, $data, env('PAYSTAR_KEY'));
+    }
+
+    /**
+     * @param int $amount
+     * @param int $order_id
+     *
+     * @return StreamInterface
+     * @throws GuzzleException
+     */
+    public function create(int $amount, int $order_id): StreamInterface
+    {
+        $body = [
+            'amount' => $amount,
+            'order_id' => $order_id,
+            'callback' => env('paystar_callback_url'),
+            'sign' => $this->sign($amount, $order_id, env('paystar_callback_url')),
+        ];
+
+        $response = $this->client->request("POST", '/create', [
+            'body' => $body
+        ]);
+
+        return $response->getBody();
     }
 
 }
