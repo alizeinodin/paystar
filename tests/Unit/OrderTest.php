f8e21bb50175b9eaa694542ea0be82ebd94e1802
alizeinodin <alizeinodin79@gmail.com>
2023-06-01 20:33:48 +0430

    OrderTest Fix. test one to many relationship between credit card's of an user and order

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\CreditCard;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,13 +12,18 @@ class OrderTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_an_order_belongs_to_a_user()
+    public function test_an_order_belongs_to_a_credit_card()
     {
         $user = User::factory()->create();
-        $order = Order::factory()->create([
+
+        $credit_card = CreditCard::factory()->create([
             'user_id' => $user->id,
         ]);
 
-        $this->assertInstanceOf(User::class, $order->user);
+        $order = Order::factory()->create([
+            'credit_card_id' => $credit_card->id
+        ]);
+
+        $this->assertInstanceOf(CreditCard::class, $order->creditCard);
     }
 }
