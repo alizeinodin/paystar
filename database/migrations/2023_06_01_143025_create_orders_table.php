45a7292b53cc74bfe4b5d11dc68c3151454f0703
alizeinodin <alizeinodin79@gmail.com>
2023-06-01 20:13:46 +0430

    credit_card_id column add in orders table as foreign key for CreditCard model

--- database/migrations/2023_06_01_143025_create_orders_table.php
+++ database/migrations/2023_06_01_143025_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\CreditCard;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -19,6 +20,7 @@ return new class extends Migration {
             $table->string('transaction_id')->nullable();
             $table->string('tracking_code')->nullable();
             $table->foreignIdFor(User::class);
+            $table->foreignIdFor(CreditCard::class);
             $table->timestamps();
         });
     }
