c2bfccbf00ef5e7c2e273e8c02c086a01ad09ab8
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 12:30:22 +0430

    callback method add in OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\OrderStatus;
 use App\Http\Requests\Order\BuyRequest;
 use App\Models\Order;
 use App\Models\Product;
 use App\Traits\Payment;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrderController extends Controller
 {
@@ -33,5 +37,45 @@ class OrderController extends Controller
             ]);
         }
         return response()->json($response);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse|void
+     * @throws GuzzleException
+     */
+    public function callback(Request $request)
+    {
+        if ($request['status'] === 1) {
+            $order = Order::where(['ref_num' => $request['ref_num']])->first();
+
+            if ($request['card_number'] !== $order->creditCard->card_number) {
+                $response = [
+                    'message' => "عملیات با شکست مواجه شد. پرداخت باید با کارتی که انتخاب کردید انجام شود. مبلغ پرداختی تا 72 ساعت آینده به حساب شما باز خواهد گشت."
+                ];
+                return response()->json($response, Response::HTTP_PAYMENT_REQUIRED);
+            }
+
+            $response = $this->verify($order['amount'],
+                $order['ref_num'],
+                $request['card_number'],
+                $request['tracking_code']);
+
+            if ($response['status'] !== 1) {
+                return response()
+                    ->json($response, Response::HTTP_FORBIDDEN);
+            }
+
+            $order->update([
+                'transaction_id' => $request['transaction_id'],
+                'tracking_code' => $request['tracking_code'],
+                'status' => OrderStatus::ACCEPTED
+            ]);
+
+            return response()
+                ->json($response, Response::HTTP_ACCEPTED);
+        }
+        # TODO implementation error handling
     }
 }
