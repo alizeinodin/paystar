7fad94a38312ef3d0d98542a5a47fbed41742753
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 12:38:47 +0430

    buy and callback route's add in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(OrderController::class)->group(function () {
+    Route::prefix('/order')->group(function () {
+        Route::name('order.')->group(function () {
+            Route::post('/buy/{product}', 'buy')
+                ->name('buy');
+            Route::post('/callback', 'callback')
+                ->name('callback');
+        });
+    });
 });
