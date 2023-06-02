b998e0b5ecb0a6f8ff99a38552ce2626b81b233e
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 13:17:46 +0430

    Store method add for store creditCard for user

--- app/Http/Controllers/CreditCardController.php
+++ app/Http/Controllers/CreditCardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreditCard\StoreRequest;
 use App\Models\CreditCard;
+use Symfony\Component\HttpFoundation\Response;
 
 class CreditCardController extends Controller
 {
@@ -12,5 +14,22 @@ class CreditCardController extends Controller
     public function index(): mixed
     {
         return CreditCard::where(['user_id' => auth('api')->user()->id])->get();
+    }
+
+    public function store(StoreRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $validatedData = $request->validated();
+
+        $creditCard = CreditCard::create([
+            'card_number' => $validatedData['card_number'],
+            'user_id' => $request->user()->id,
+        ]);
+
+        $response = [
+            'message' => 'کارت بانکی با موفقیت افزوده شد',
+            'creditCard' => $creditCard,
+        ];
+
+        return response()->json($response, Response::HTTP_CREATED);
     }
 }
