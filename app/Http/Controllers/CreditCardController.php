1a848cf085d5910c81dd57978d3646d554a05e9a
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 12:49:50 +0430

    index method add. this method return all CreditCard of a user

--- app/Http/Controllers/CreditCardController.php
+++ app/Http/Controllers/CreditCardController.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CreditCard;
+
 class CreditCardController extends Controller
 {
+    /**
+     * @return mixed
+     */
+    public function index(): mixed
+    {
+        return CreditCard::where(['user_id' => auth('api')->user()->id])->get();
+    }
 }
