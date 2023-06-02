c4c525a2ece6166ab51d1bc16f3a0e9924997596
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 19:25:36 +0430

    get mehtod add in ProductController for get a product by id

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
     public function index(): Collection
     {
         return Product::all();
+    }
+
+    public function get(Product $product): JsonResponse
+    {
+        return response()->json($product, Response::HTTP_OK);
     }
 }
