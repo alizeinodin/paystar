9c4df87147f33e43eeff1c94e20b642d4e03074f
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 18:57:03 +0430

    ProductController add

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+
+class ProductController extends Controller
+{
+    public function index(): Collection
+    {
+        return Product::all();
+    }
+}
