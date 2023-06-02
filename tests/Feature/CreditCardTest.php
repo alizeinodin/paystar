5618bab211b994a552f28b613e90ddd348ac237c
alizeinodin <alizeinodin79@gmail.com>
2023-06-02 14:25:35 +0430

    test of store a credit card add

--- tests/Feature/CreditCardTest.php
+++ tests/Feature/CreditCardTest.php
@@ -2,13 +2,38 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class CreditCardTest extends TestCase
 {
+    use WithFaker;
+
+    private function getUser()
+    {
+        return User::factory()->create();
+    }
+
     public function test_dont_allowed_as_a_guest()
     {
         $response = $this->getJson(route('card.all'));
         $response->assertStatus(401);
+    }
+
+    public function test_store_a_credit_card()
+    {
+        $user = $this->getUser();
+        Sanctum::actingAs($user);
+
+        $cardNumber = $this->faker->creditCardNumber;
+
+        $request = [
+            'card_number' => $cardNumber,
+        ];
+
+        $response = $this->postJson(route('card.store'), $request);
+        $response->assertCreated();
     }
 }
