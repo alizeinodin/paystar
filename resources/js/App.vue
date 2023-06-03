c18e7ff63f8ca636b33ad8d169a11e206924766e
alizeinodin <alizeinodin79@gmail.com>
2023-06-03 18:01:22 +0430

    Navbar add to app.vue and fontface add to project

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,22 +1,35 @@
 <template>
     <div id="app">
+        <Navbar></Navbar>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        Navbar,
+    }
 }
 </script>
 
-<style scoped>
+<style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'IRANSans', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+@font-face {
+    font-family: IRANSans;
+    font-weight: normal;
+    src: url("./fonts/eot/IRANSansWeb.eot") format("embedded-opentype"),
+    url("./fonts/ttf/IRANSansWeb.ttf") format("truetype"),
+    url("./fonts/woff/IRANSansWeb.woff") format("woff");
 }
 </style>
