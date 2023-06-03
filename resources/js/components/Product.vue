4b143a593d66879b42c24174d3b9665e19806605
alizeinodin <alizeinodin79@gmail.com>
2023-06-03 18:03:58 +0430

    Product component add

---
+++ resources/js/components/Product.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-card
+        class="mx-auto"
+        max-width="344"
+    >
+        <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+            cover
+        ></v-img>
+
+        <v-card-title>
+            {{ title }}
+        </v-card-title>
+
+        <v-card-subtitle>
+            قیمت: {{ price }}
+        </v-card-subtitle>
+
+        <v-card-actions>
+            <v-btn
+                color="orange-lighten-2"
+                variant="text"
+            >
+                خرید
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+                :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                @click="show = !show"
+            ></v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+            <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                    {{ body }}
+                </v-card-text>
+            </div>
+        </v-expand-transition>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "Product",
+    data: () => ({
+        show: false
+    }),
+    props: ['title', 'body', 'price']
+}
+</script>
+
+<style scoped>
+
+</style>
