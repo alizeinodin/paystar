c4360310944fb6caa14c0adea06d52418f326ca6
alizeinodin <alizeinodin79@gmail.com>
2023-06-03 21:12:06 +0430

    CreditCard component add

---
+++ resources/js/components/CreditCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card variant="tonal" prepend-icon="mdi-credit-card-check">
+        <template v-slot:title>
+            {{ card_number }}
+        </template>
+        <v-card-actions>
+            <v-btn>انتخاب کنید</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "CreditCard",
+    props: ['card_number']
+}
+</script>
+
+<style scoped>
+
+</style>
