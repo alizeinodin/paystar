ccbd96bc1f2e3cb49976350ffd9250f9ea7d5546
alizeinodin <alizeinodin79@gmail.com>
2023-06-03 18:02:20 +0430

    import mdi/font uncommment

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,7 @@ import App from './App.vue'
 import router from './router/index.js'
 
 // vuetify
-// import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
