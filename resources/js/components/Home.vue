af35ca3c087d7a071144e92212b6e64cd2e3d9b5
alizeinodin <alizeinodin79@gmail.com>
2023-06-03 18:03:16 +0430

    Grid and axios add to Home.vue

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,10 +1,43 @@
 <template>
-    <div>home</div>
+    <v-container>
+        <v-row no-gutters>
+            <v-col
+                v-for="n in 1"
+                :key="n"
+                cols="12"
+                sm="4"
+            >
+                <Product title="test" price="2000" body="test hello"></Product>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+import Product from './Product.vue'
+
 export default {
-    name: "Home"
+    name: "Home",
+    setup() {
+        axios({
+            method: 'get',
+            url: 'https://kiwi.ssceb.ir/api/product/all',
+            timeout: 20000,
+            headers: {
+                'Accept': 'application/json',
+            }
+
+        })
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (response) {
+                console.log(response)
+            })
+    },
+    components: {
+        Product
+    }
 }
 </script>
 
