d4adc3517f12b5c1d84fe50afb87fc7f144ccef9
alizeinodin <alizeinodin79@gmail.com>
2023-06-03 17:15:01 +0430

    Login and Register routes add

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,10 +1,20 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../components/Home.vue'
+import Register from '../components/Register.vue'
+import Login from '../components/Login.vue'
 
 const routes = [
     {
         path: '/',
         component: Home
+    },
+    {
+        path: '/register',
+        component: Register
+    },
+    {
+        path: '/login',
+        component: Login
     }
 ]
 
