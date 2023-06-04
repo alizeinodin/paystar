aa9816854e9a01a620ed2e27e1613dcc6a7a5355
alizeinodin <alizeinodin79@gmail.com>
2023-06-04 15:01:16 +0430

    css and js add in home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PayStar</title>
-
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('/build/assets/app-28d2a370.css') }} ">
+    <link rel="stylesheet" href="{{ asset('/build/assets/app-bbd6a014.css') }} ">
 </head>
 <body>
 <div id="app">
 
 </div>
 
-@vite('resources/js/app.js')
+<script src="{{ asset('build/assets/app-52c2e43d.js') }}"></script>
 </body>
 </html>
