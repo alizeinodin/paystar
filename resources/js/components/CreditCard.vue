263aa39607890f6d646fbb920366c7ee144e9623
alizeinodin <alizeinodin79@gmail.com>
2023-06-04 11:44:12 +0430

    selectCard method add

--- resources/js/components/CreditCard.vue
+++ resources/js/components/CreditCard.vue
@@ -4,7 +4,7 @@
             {{ card_number }}
         </template>
         <v-card-actions>
-            <v-btn>انتخاب کنید</v-btn>
+            <v-btn @click="selectCard()">انتخاب کنید</v-btn>
         </v-card-actions>
     </v-card>
 </template>
@@ -12,7 +12,34 @@
 <script>
 export default {
     name: "CreditCard",
-    props: ['card_number']
+    props: ['card_number', 'id', 'product_id'],
+    data: () => ({
+        data: {}
+    }),
+    methods: {
+        selectCard() {
+            const token = localStorage.getItem('token')
+
+            axios({
+                method: 'post',
+                url: "https://kiwi.ssceb.ir/api/order/buy/" + this.product_id,
+                timeout: 20000,
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                },
+                data: {
+                    card_id: this.id
+                }
+            }).then((response) => {
+                console.log(response)
+                this.data = response.data
+                this.$emit('GoToBank', this.data)
+            }).catch((response) => {
+                console.log(response)
+            })
+        }
+    }
 }
 </script>
 
